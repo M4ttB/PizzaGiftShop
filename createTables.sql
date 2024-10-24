8268c9ffe00a2597347a8e5a2980d0d72474a23c
M4ttB <m4tt.bunch@gmail.com>
2024-10-24 16:09:44 -0700

    Update createTables.sql
    Add scp_items table for hidden order information not displayed on the site but emailed to customer in encrypted form when order is finalized.

--- createTables.sql
+++ createTables.sql
@@ -1,3 +1,4 @@
+/*Order table holds customer's info along with the total of the invoice.*/
 CREATE TABLE orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -7,6 +8,7 @@ CREATE TABLE orders (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+/*Represents the gift shop items as displayed on the webpage.*/
 CREATE TABLE order_items (
     id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
@@ -15,4 +17,13 @@ CREATE TABLE order_items (
     quantity INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(id)
+);
+
+/*When orders are filled, information from this table are emailed as an encrypted invoice. In other words, this table represents the true order items.*/
+CREATE TABLE scp_items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    scp_id INT,
+    scp_classification VARCHAR,
+    description TEXT NOT NULL,
+    FOREIGN KEY (scp_id) REFERENCES order_items(item_id)
 );
