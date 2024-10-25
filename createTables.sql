9212e5d85548b07adf6258d5a95cc1f01b97ac1b
M4ttB <m4tt.bunch@gmail.com>
2024-10-25 13:21:46 -0700

    Update createTables.sql
    Added new table for giftshop items.
    
    order_items is separate from giftshop_items and scp_items
    
    giftshop_items and scp_items share the same id
    
    Sample insert statements for giftshop_items and  scp_items

--- createTables.sql
+++ createTables.sql
@@ -3,27 +3,47 @@ CREATE TABLE orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     phone VARCHAR(50) NOT NULL,
-    address TEXT NOT NULL,
+    address VARCHAR(200) NOT NULL,
     total DECIMAL(10, 2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-/*Represents the gift shop items as displayed on the webpage.*/
+/*Represents the order items in giftshop shopping cart.*/
 CREATE TABLE order_items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    item_id INT,
-    description TEXT NOT NULL,
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    item_id SMALLINT,
     quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(id)
 );
 
+/*Presents the items that can be ordered from the giftshop*/
+CREATE TABLE giftshop_items (
+    item_id SMALLINT PRIMARY KEY,
+    name VARCHAR,
+    description VARCHAR,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (item_id) REFERENCES order_items(item_id)
+);
+    
+
 /*When orders are filled, information from this table are emailed as an encrypted invoice. In other words, this table represents the true order items.*/
 CREATE TABLE scp_items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    scp_id INT,
-    scp_classification VARCHAR,
-    description TEXT NOT NULL,
+    scp_id SMALLINT PRIMARY KEY, /*SCP IDs go up to around 10,000 so no need for higher values*/
+    scp_classification VARCHAR(30),
+    description VARCHAR,
     FOREIGN KEY (scp_id) REFERENCES order_items(item_id)
 );
+
+/*Sample Giftshop Item Insert Statement*/
+INSERT INTO giftshop_items (item_id, description, price) VALUES ('8593', 'Pasta with Fazool - Collector's Edition VHS Tape', 'Enjoy this blast from the past 90's family Italian cooking show, featuring everyone's favorite mascot, Fazool Catavelli! Children approved and puts a smile on Grandma's face!', 22.99);
+
+/*Sample SCP Item Insert Statement (Source: https://scp-wiki.wikidot.com/scp-8593)*/
+INSERT INTO scp_items (scp_id, scp_classification, description) VALUES ('8593', 'EUCLID', 'SCP-8593 is a series of television broadcasts for "Pasta with Fazool," a cooking show focused on Italian American cuisine.
+
+SCP-8593-1 hosts SCP-8593 and is a victual humanoid entity self-identifying as "Fazool Cavatelli." SCP-8593-1's body is composed entirely of dry pasta; its torso is constructed from a bundle of spaghetti, while two pieces of penne form its arms. SCP-8593-1 possesses human hands, which are incongruent with the rest of its composition and each other. Its head is a large meatball, capable of limited facial expressions and speech in English. SCP-8593-1's outfit changes between episodes, though these changes appear limited to commonly colored or patterned aprons and matching toques.
+
+SCP-8593-1 appears on SCP-8593 as a poorly keyframe-animated puppet, a characteristic never acknowledged by it or any guests who treat it as a baseline human while also recognizing that it's composed entirely of food.
+
+It moves with jerky, unnatural movements, with parts of its body often disconnecting from each other. SCP-8593-1's hands constantly change size, shape, and skin color to accommodate different gestures or movements. Despite this, SCP-8593 can interact with physical objects, including human guests and various cooking utensils. Lighting does not affect SCP-8593-1, giving it a flat, two-dimensional appearance at all times.
+
+Despite SCP-8593-1 claiming SCP-8593 is focused on Italian American cuisine, all presented materials, including recipes, origins of said recipes, and Italian history differ significantly from any traditions known on Earth. No source for SCP-8593 broadcasts has been discovered.');
